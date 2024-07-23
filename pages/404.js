f2b694e96f70096d16098459c79bd9e52f27f128
Ivan R. Chenoweth <ivanchenoweth@gmail.com>
2024-07-22 17:22:23 -0700

    fix 404 page

--- pages/404.js
+++ pages/404.js
@@ -5,7 +5,7 @@ const NotFound = () => {
     <div className="not-found">
       <h1>Ooops...</h1>
       <h2>That page cannot be found :(</h2>
-      <p>Go back to the <Link href="/"><a>Homepage</a></Link></p>
+      <p>Go back to the <Link href="/">Homepage</Link></p>
     </div>
   );
 }
